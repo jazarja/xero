a7decd4ffe601b26a3be74da6904feec2d0d0055
jazarja <jazarja@yahoo.com>
2016-09-14 08:51:35 +0700

    Add attach method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var crypto  = require("crypto");
-var oauth   = require("oauth");
+var crypto = require("crypto");
+var oauth = require("oauth");
 var EasyXml = require('easyxml');
 var xml2js = require('xml2js');
 var inflect = require('inflect');
@@ -11,16 +11,20 @@ function Xero(key, secret, rsa_key, showXmlAttributes, customHeaders) {
     this.key = key;
     this.secret = secret;
 
-    this.parser = new xml2js.Parser({explicitArray: false, ignoreAttrs: showXmlAttributes !== undefined ? (showXmlAttributes ? false : true) : true, async: true});
+    this.parser = new xml2js.Parser({
+        explicitArray: false,
+        ignoreAttrs: showXmlAttributes !== undefined ? (showXmlAttributes ? false : true) : true,
+        async: true
+    });
 
     this.oa = new oauth.OAuth(null, null, key, secret, '1.0', null, "PLAINTEXT", null, customHeaders);
     this.oa._signatureMethod = "RSA-SHA1"
-    this.oa._createSignature = function(signatureBase, tokenSecret) {
+    this.oa._createSignature = function (signatureBase, tokenSecret) {
         return crypto.createSign("RSA-SHA1").update(signatureBase).sign(rsa_key, output_format = "base64");
     }
 }
 
-Xero.prototype.call = function(method, path, body, callback) {
+Xero.prototype.call = function (method, path, body, callback) {
     var self = this;
 
     var post_body = null;
@@ -30,16 +34,20 @@ Xero.prototype.call = function(method, path, body, callback) {
             post_body = body;
         } else {
             var root = path.match(/([^\/\?]+)/)[1];
-            post_body = new EasyXml({rootElement: inflect.singularize(root), rootArray: root, manifest: true}).render(body);
+            post_body = new EasyXml({
+                rootElement: inflect.singularize(root),
+                rootArray: root,
+                manifest: true
+            }).render(body);
             content_type = 'application/xml';
         }
     }
-    var process = function(err, xml, res) {
+    var process = function (err, xml, res) {
         if (err) {
             return callback(err);
         }
 
-        self.parser.parseString(xml, function(err, json) {
+        self.parser.parseString(xml, function (err, json) {
             if (err) return callback(err);
             if (json && json.Response && json.Response.Status !== 'OK') {
                 return callback(json, res);
@@ -49,6 +57,35 @@ Xero.prototype.call = function(method, path, body, callback) {
         });
     };
     return self.oa._performSecureRequest(self.key, self.secret, method, XERO_API_URL + path, null, post_body, content_type, callback ? process : null);
+}
+
+Xero.prototype.attach = function (method, path, filename, callback) {
+    var self = this;
+
+    var post_body = null;
+    var content_type = null;
+
+    content_type = 'application/pdf';
+
+    require('fs').readFile(filename, function (err, post_body) {
+        if (err) throw err;
+        var process = function (err, xml, res) {
+            if (err) {
+                return callback(err);
+            }
+            self.parser.parseString(xml, function (err, json) {
+                if (err) return callback(err);
+                if (json && json.Response && json.Response.Status !== 'OK') {
+                    return callback(json, res);
+                } else {
+                    return callback(null, json, res);
+                }
+            });
+        };
+
+        return self.oa._performSecureRequest(self.key, self.secret, method, XERO_API_URL + path, null, post_body, content_type, callback ? process : null);
+    });
+
 }
 
 module.exports = Xero;
